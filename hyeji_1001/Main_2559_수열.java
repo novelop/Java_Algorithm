c2c036eb2ac86db68a7081adbfdb384c09674f29
야끼 <noveloper97@gmail.com>
2022-10-05 01:19:23 +0900

    1001

--- hyeji_1001/Main_2559_수열.java
+++ hyeji_1001/Main_2559_수열.java
@@ -1,22 +1,37 @@
 package hyeji_1001;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_2559_수열 {
-
-	public static void main(String[] args) {
-		int N = 10;
-		int K = 5;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int cnt = 0;
-		int sum = 0;
-		for(int i= N; i > 0; i--) {
-			if(sum + i <= K) {
-				sum += i;
-				cnt++;
-			}
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] input = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
 		}
-		if(sum != K) cnt = -1;
-
-		System.out.println(cnt);
+		
+		
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for(int i=0; i<=N-K; i++) {
+			sum = 0;
+			for(int j=i; j<i+K; j++) {
+				sum += input[j];
+			}
+			max = Math.max(max, sum);
+		}
+			
+		System.out.println(max);
 		
 	}
 
